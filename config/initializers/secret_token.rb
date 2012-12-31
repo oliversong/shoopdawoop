7162ad9573089db0f9f33862e56c22ac0ac82c14
oliversong <oliversong@tumblr.com>
2012-12-31 01:45:37 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoopdawoop::Application.config.secret_token = '90d6a8e02196df7f49428978e3a46075647967ef6d77fce309a5a1266cac83119f1e18e8561a88c631e671067249b4eb6f9d07f8467e71d41ff71da66493cb80'
